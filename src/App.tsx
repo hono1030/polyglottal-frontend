7d8db1a62cdf1c4652c1799ecdce37892f304cca
hono1030 <honoka.n1030@gmail.com>
2024-11-12 22:58:26 +0900

    feat : implement username input

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,46 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, ReactNode } from "react";
 import "./App.css";
+import "./components/Login";
+import Login from "./components/Login";
 const wsUrl: string = import.meta.env.VITE_WS_URL;
 
 function App() {
-  const [clientId, setClientId] = useState(
+  const [clientId, setClientId] = useState<ReactNode>(
     Math.floor(new Date().getTime() / 1000)
   );
-  const [websocket, setWebsocket] = useState<string>();
-  const [message, setMessage] = useState([]);
+  const [user, setUser] = useState<string | null>(null);
+  const [websocket, setWebsocket] = useState<WebSocket>();
+  const [message, setMessage] = useState<object[]>([]);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    // const url = `${wsUrl}/ws/${clientId}`;
-    const url = "ws://localhost:8000/ws/" + clientId;
+    const url = `${wsUrl}/ws/${user}/${clientId}`;
     const ws = new WebSocket(url);
 
-    ws.onopen = () => {
-      ws.send("Connect");
-      console.log("WebScoket connection established");
-    };
+    if (user) {
+      ws.onopen = () => {
+        // ws.send("Connect");
+        ws.send(`${user} joined the chat`);
+        console.log("WebScoket connection established");
+      };
 
-    // recieve message every start page
-    ws.onmessage = (e) => {
-      const message = JSON.parse(e.data);
-      setMessages((prevMessages) => [...prevMessages, message]);
-    };
+      // recieve message every start page
+      ws.onmessage = (e) => {
+        const message = JSON.parse(e.data);
+        setMessages((prevMessages) => [...prevMessages, message]);
+      };
 
-    ws.onclose = () => {
-      console.log("WebSocket connection closed");
-    };
+      ws.onclose = () => {
+        console.log("WebSocket connection closed");
+      };
 
-    setWebsocket(ws);
-    // clean up function when we close page
-    return () => {
-      ws.close();
-    };
-  }, []);
+      setWebsocket(ws);
+      // clean up function when we close page
+      return () => {
+        ws.close();
+      };
+    }
+  }, [user, clientId]);
 
   const sendMessages = () => {
     if (websocket && websocket.readyState === WebSocket.OPEN) {
@@ -49,44 +54,51 @@ function App() {
   return (
     <div className="container">
       <h1>Chat</h1>
-      <h2>Your client id: {clientId}</h2>
-      <div className="chat-container">
-        <div className="chat">
-          {messages.map((value, index) => {
-            if (value.clientId === clientId) {
-              return (
-                <div key={index} className="my-message-container">
-                  <div className="my-message">
-                    <p className="client">client id : {clientId}</p>
-                    <p className="message">{value.message}</p>
-                  </div>
-                </div>
-              );
-            } else {
-              return (
-                <div key={index} className="another-message-container">
-                  <div className="another-message">
-                    <p className="client">client id : {clientId}</p>
-                    <p className="message">{value.message}</p>
-                  </div>
-                </div>
-              );
-            }
-          })}
-        </div>
-        <div className="input-chat-container">
-          <input
-            className="input-chat"
-            type="text"
-            placeholder="Chat message ..."
-            onChange={(e) => setMessage(e.target.value)}
-            value={message}
-          ></input>
-          <button className="submit-chat" onClick={sendMessages}>
-            Send
-          </button>
-        </div>
-      </div>
+
+      {!user ? (
+        <Login setUser={setUser}></Login>
+      ) : (
+        <>
+          <h2>Your username: {user}</h2>
+          <div className="chat-container">
+            <div className="chat">
+              {messages.map((value, index) => {
+                if (value.clientId === clientId) {
+                  return (
+                    <div key={index} className="my-message-container">
+                      <div className="my-message">
+                        <p className="client">{user}</p>
+                        <p className="message">{value.message}</p>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={index} className="another-message-container">
+                      <div className="another-message">
+                        <p className="client">{value.username}</p>
+                        <p className="message">{value.message}</p>
+                      </div>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+            <div className="input-chat-container">
+              <input
+                className="input-chat"
+                type="text"
+                placeholder="Chat message ..."
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              ></input>
+              <button className="submit-chat" onClick={sendMessages}>
+                Send
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, useRef, ReactNode } from "react";
+import axios from "axios";
+import "../App.css";
+const apiUrl: string = import.meta.env.VITE_API_URL;
+
+type Props = {
+  setUser: (userData: string) => void;
+};
+
+const Login: React.FC<Props> = ({ setUser }) => {
+  const [username, setUsername] = useState<string>("");
+
+  const sendUsername = async () => {
+    setUsername("");
+    try {
+      const response = await axios.post(`${apiUrl}/login`, {
+        username: username,
+      });
+
+      const userData = response.data;
+      setUser(userData.username);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Enter your username</h2>
+      <input
+        className="username-input"
+        type="text"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+      ></input>
+      <button className="submit-username" onClick={sendUsername}>
+        Enter the chat
+      </button>
+    </div>
+  );
+};
+
+export default Login;
